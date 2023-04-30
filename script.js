61b302604eba67f0404fb077f96d7482731c3928
Daniel Qolami <qolamidaniel@gmail.com>
2023-04-30 04:37:25 +0430

    changed api variable

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let photosArray = [];
 
 // Unsplash API
 const count = 30;
-const apiKey = "YOUR_API_KEY_HERE";
+const apiKey = process.env.UNSPLASH_API_KEY;
 const query = "girl in nature";
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=${query}`;
 
